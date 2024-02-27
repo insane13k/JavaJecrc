970b371d51fc2e30bc8acbd56e4445984d6abd67
Aryan Arora <74876656+insane13k@users.noreply.github.com>
2024-02-27 22:42:18 +0530

    Added File

---
+++ test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class test
+{
+	int id;
+	String name;
+}
+
+class hello
+{
+	public static void main(String args[])
+	{
+		test obj=new test();
+		obj.id=150;
+		obj.name="Aryan";
+		System.out.println(obj.id);
+		System.out.println(obj.name);
+	}
+}
+		
+			
